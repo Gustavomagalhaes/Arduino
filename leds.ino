0c209468d8c4ded656bd8d124c9af77c50cf7635
Gustavo Magalhães <itsgustavopereira@gmail.com>
2015-06-03 15:33:17 -0300

    My first code with 3 leds

---
+++ leds.ino
@@ -0,0 +1,25 @@
+//http://www.smashingrobotics.com/arduino-simulators-lineup-start-developing-without-real-board/
+
+double time = 200;
+int LED1 = 2;
+int LED2 = 3;
+int LED3 = 4;
+
+void setup() {
+  pinMode(2, OUTPUT); // RED
+  pinMode(3, OUTPUT); // YELLOW
+  pinMode(4, OUTPUT); // GREEN
+}
+
+void loop() {
+  blinky(LED1);
+  blinky(LED2);
+  blinky(LED3);
+  blinky(LED2);
+}
+
+void blinky (int LED) {
+  digitalWrite(LED, HIGH);
+  delay(time);
+  digitalWrite(LED, LOW);
+}
